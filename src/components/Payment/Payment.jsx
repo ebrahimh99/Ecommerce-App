5f27bda1ef92bbf44cdccfddb35d125f32357aaa
ebrahimh99 <ebrahimh152@gmail.com>
2025-04-30 21:45:00 +0300

    Fix payment redirect URL 2

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -62,7 +62,7 @@ const Payment = () => {
 
         try {
             const { data } = await axios.post(
-                `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=${encodeURIComponent(redirectUrl)}`,
+                `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://ebrahimh99.github.io/Ecommerce-App/#/allorders`,
                 cashOrderObject,
                 {
                     headers: {
